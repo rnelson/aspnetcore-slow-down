e106ccca854896d953f7083facf65045869ee101
Ross Nelson <ross.nelson@gmail.com>
2024-11-20 13:33:59 -0600

    Alphabetize the headers

--- src/SlowDown/Constants.cs
+++ src/SlowDown/Constants.cs
@@ -2,9 +2,9 @@
 
 internal static class Constants
 {
+    public const string DelayHeader = "X-Slow-Down-Delay";
     public const string LimitHeader = "X-Slow-Down-Limit";
     public const string RemainingHeader = "X-Slow-Down-Remaining";
-    public const string DelayHeader = "X-Slow-Down-Delay";
     
     public const string ConfigurationKey = "SlowDown";
 }
