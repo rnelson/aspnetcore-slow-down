0745b2007e9f663c4ee42626a71f4e1427487f3c
Ross Nelson <ross.nelson@gmail.com>
2024-11-20 13:45:20 -0600

    Test middleware extensions

---
+++ test/SlowDown.Tests/SlowDownMiddlewareExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Nearform.AspNetCore.SlowDown;
+
+namespace SlowDown.Tests;
+
+public class SlowDownMiddlewareExtensionsTests
+{
+    private static readonly SemaphoreSlim Semaphore = new(1,1);
+    
+    [Fact]
+    public async Task MiddlewareExtensions_Work()
+    {
+        await Semaphore.WaitAsync();
+
+        try
+        {
+            var builder = WebApplication.CreateBuilder();
+            var services = builder.Services;
+
+            services.AddSlowDown(config =>
+            {
+                config.Cache = UnitTestHelperMethods.CreateCache();
+            });
+
+            await using var app = builder.Build();
+
+            app.UseRouting();
+            app.UseSlowDown();
+        }
+        finally
+        {
+            Semaphore.Release();
+        }
+    }
+}
