4d3fda56591730b0359379437b76ad367f51b571
Ross Nelson <ross.nelson@gmail.com>
2024-12-05 12:53:45 -0600

    Allow the caller to send in a cancellation token

--- src/SlowDown/Helpers/CacheHelper.cs
+++ src/SlowDown/Helpers/CacheHelper.cs
@@ -12,17 +12,21 @@ public class CacheHelper(SlowDownOptions options, HybridCache cache)
     private readonly SlowDownOptions _options = options ?? throw new ArgumentNullException(nameof(options));
     private readonly HybridCache _cache = cache ?? throw new ArgumentNullException(nameof(cache));
     
-    public async Task<int> Get(HttpRequest request, IEnumerable<string>? tags = null)
+    public async Task<int> Get(HttpRequest request,
+        IEnumerable<string>? tags = null,
+        CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
         var key = await _options.KeyGenerator(request, ct);
 
         return await Get(key, tags);
     }
 
-    public async Task<int> Get(string key, IEnumerable<string>? tags = null)
+    public async Task<int> Get(string key,
+        IEnumerable<string>? tags = null,
+        CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
         
         var cacheOptions = new HybridCacheEntryOptions
         {
@@ -40,37 +44,43 @@ public class CacheHelper(SlowDownOptions options, HybridCache cache)
         return count;
     }
 
-    public async Task Remove(HttpRequest request)
+    public async Task Remove(HttpRequest request, CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
         var key = await _options.KeyGenerator(request, ct);
         
         await _cache.RemoveAsync(key, cancellationToken: ct);
     }
 
-    public async Task Remove(string key)
+    public async Task Remove(string key, CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
         await _cache.RemoveAsync(key, cancellationToken: ct);
     }
 
-    public async Task RemoveAll(IEnumerable<string> tags)
+    public async Task RemoveAll(IEnumerable<string> tags, CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
         await _cache.RemoveByTagAsync(tags, cancellationToken: ct);
     }
     
-    public async Task Set(HttpRequest request, int value, IEnumerable<string>? tags = null)
+    public async Task Set(HttpRequest request,
+        int value,
+        IEnumerable<string>? tags = null,
+        CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
         var key = await _options.KeyGenerator(request, ct);
 
         await Set(key, value, tags);
     }
 
-    public async Task Set(string key, int value, IEnumerable<string>? tags = null)
+    public async Task Set(string key,
+        int value,
+        IEnumerable<string>? tags = null,
+        CancellationToken? cancellationToken = null)
     {
-        var ct = GetCancellationToken();
+        var ct = cancellationToken ?? GetCancellationToken();
 
         var cacheOptions = new HybridCacheEntryOptions
         {
